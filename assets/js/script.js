3c9829b18a07142928da3c61a293eae98eae168e
LisaMolloy5 <molloylisa5@gmail.com>
2022-03-06 12:21:59 +0000

    Feat check if two cards have been clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
 let cards = document.querySelectorAll('.card')
 
+let cardFlipped = false;
+let firstCard, secondCard;
 
 function flipCards() {
 
     this.classList.toggle('flip')
 
-    
-
-}
-
-cards.forEach(card => card.addEventListener('click', flipCards))
-
-function checkMatch() {
-
+    if (!cardFlipped) {
+        // First card clicked 
+        cardFlipped = true;
+        firstCard = this;
+    } else {
+        // Second card clicked
+        cardFlipped = false;
+        secondCard = this;
+    }
 }
 
 function unflipCard() {
 
 }
 
+function checkMatch() {
+
+}
+
 function shuffle() {
 
 }
+
+cards.forEach(card => card.addEventListener('click', flipCards))
