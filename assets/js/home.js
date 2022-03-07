2d3f970404d2db6377d087b5f94c323f50f0ff74
LisaMolloy5 <molloylisa5@gmail.com>
2022-03-07 11:33:13 +0000

    Feat how to play instructions

---
+++ assets/js/home.js
@@ -0,0 +1,15 @@
+const howBtn = document.getElementById('how-btn')
+const gameIns = document.getElementById('instructions')
+const closeBtn = document.getElementById('close')
+
+howBtn.addEventListener('click', howToPlay)
+closeBtn.addEventListener('click', closeIns)
+
+function howToPlay() {
+  gameIns.classList.remove('hide')
+}
+
+function closeIns() {
+    gameIns.classList.add('hide')
+}
+
